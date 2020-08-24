872660928bc031ac22cd6db67414e9478ccd06e9
Louis <louis.zerri@hotmail.fr>
2020-08-24 23:15:44 +0200

    Filtre symfony

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Data\SearchData;
+use App\Form\SearchForm;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/", name="product")
+     * @param ProductRepository $productRepository
+     * @param Request $request
+     * @return Response
+     */
+    public function index(ProductRepository $productRepository, Request $request)
+    {
+        $data = new SearchData();
+        $data->page = $request->get('page', 1);
+        $form = $this->createForm(SearchForm::class, $data);
+
+        $form->handleRequest($request);
+
+        $products = $productRepository
+            ->findSearch($data);
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+            'form' => $form->createView()
+        ]);
+    }
+}
