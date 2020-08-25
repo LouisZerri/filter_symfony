f36718fdf4845df225b69a4089fe7ba90f0d2151
Louis <louis.zerri@hotmail.fr>
2020-08-25 02:38:12 +0200

    Premiere partie filtre ajax

---
+++ assets/js/modules/Filter.js
@@ -0,0 +1,87 @@
+/**
+ * @property {HTMLElement} pagination
+ * @property {HTMLElement} content
+ * @property {HTMLElement} sorting
+ * @property {HTMLFormElement} form
+ */
+export default class Filter {
+
+    /**
+     *
+     * @param {HTMLElement|null} element
+     */
+    constructor(element) {
+
+        if(element === null)
+        {
+            return;
+        }
+
+        this.pagination = element.querySelector('.js-filter-pagination')
+        this.content = element.querySelector('.js-filter-content')
+        this.sorting = element.querySelector('.js-filter-sorting')
+        this.form = element.querySelector('.js-filter-form')
+        this.bindEvent();
+
+    }
+
+    /**
+     * Ajoute les comportements aux différents éléments
+     */
+    bindEvent()
+    {
+        this.sorting.addEventListener('click', e => {
+            if(e.target.tagName === 'A')
+            {
+                e.preventDefault();
+                this.loadUrl(e.target.getAttribute('href'))
+            }
+        })
+
+        this.form.querySelectorAll('input[type="checkbox"]').forEach(input =>{
+            input.addEventListener('change', this.loadForm.bind(this))
+        })
+
+    }
+
+    async loadUrl(url)
+    {
+        const ajaxUrl = url + '&ajax=1'
+
+
+        const response = await fetch(ajaxUrl, {
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        })
+
+        if(response.status >= 200 && response.status < 300)
+        {
+            const data = await response.json()
+            this.content.innerHTML = data.content
+            this.sorting.innerHTML = data.sorting
+            history.replaceState({},'', url)
+        }
+        else
+        {
+            console.error(response)
+        }
+    }
+
+    async loadForm()
+    {
+        const data = new FormData(this.form)
+        const url = new URL(this.form.getAttribute('action') || window.location.href)
+        const params = new URLSearchParams()
+
+        data.forEach((value, key) => {
+            params.append(key, value);
+        })
+
+        return this.loadUrl(url.pathname + '?' + params.toString())
+    }
+
+
+
+
+}
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,6 +6,7 @@ use App\Data\SearchData;
 use App\Form\SearchForm;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,12 +27,24 @@ class ProductController extends AbstractController
 
         $form->handleRequest($request);
 
+        [$min, $max] = $productRepository->findMinMax($data);
+
         $products = $productRepository
             ->findSearch($data);
 
+        if($request->get('ajax'))
+        {
+            return new JsonResponse([
+                'content' => $this->renderView('product/_products.html.twig', ['products' => $products]),
+                'sorting' => $this->renderView('product/_sorting.html.twig', ['products' => $products])
+            ]);
+        }
+
         return $this->render('product/index.html.twig', [
             'products' => $products,
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'min' => $min,
+            'max' => $max
         ]);
     }
 }
